2fe813225664be7597cd1c6198f2b4115e196fb5
atlanwaserver <atlanwa.server@gmail.com>
2024-06-12 11:36:34 +0530

    updated logo

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,5 @@
+<img
+    class="h-16"
+    src="{{ asset('images/logo.png') }}"
+    alt = "ALSTOM"
+/>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 });
 Livewire::setUpdateRoute(function ($handle) {
     return Route::post('/Task-Sequence/public/livewire/update', $handle);
